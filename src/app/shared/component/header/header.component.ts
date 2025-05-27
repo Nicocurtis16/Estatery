41dc7d3fafefd78976eaef47b461ce4ae0ce0711
Nicocurtis16 <nicholaskyereboah@gmail.com>
2025-05-27 14:06:09 +0000

    feat: refactor header and menus components; remove unused files and improve structure

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,9 +1,8 @@
-import { Component } from "@angular/core";
-import { MenusComponent } from "../../ui/menus.component";
+import { Component } from '@angular/core';
+import { MenusComponent } from '../../ui/menus/menus.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   imports: [MenusComponent],
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
---
+++ src/app/shared/ui/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+})
+export class ButtonComponent {
+  @Input() state: 'active' | 'hover' | 'disable' = 'active';
+  @Input() style: 'fill' | 'outlined' | 'text' | 'small-text' = 'fill';
+  @Input() color: 'primary' | 'secondary' | 'tertiary' = 'primary';
+  @Input() prefixIcon?: string;
+  @Input() suffixIcon?: string;
+  @Input() text: boolean = true;
+  @Output() onClick = new EventEmitter<Event>();
+}
